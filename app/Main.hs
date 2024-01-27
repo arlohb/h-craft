14478257e3869b1700e48ec028e2e0163af07955
arlohb <arlo@urwick.co.uk>
2024-01-27 15:23:05 +0000

    Now creating cube manually

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,92 @@ import Raylib.Util
 import Raylib.Util.Colors
 import Raylib.Types
 import Data.Functor ((<&>))
+import Data.Word (Word16)
+
+data Direction = Px | Py | Pz | Nx | Ny | Nz
+directions :: [Direction]
+directions = [Px, Py, Pz, Nx, Ny, Nz]
+
+faceVerts :: Direction -> [Vector3]
+faceVerts Px = [
+    Vector3 1 0 0,
+    Vector3 1 1 0,
+    Vector3 1 1 1,
+    Vector3 1 0 1
+    ]
+faceVerts Py = [
+    Vector3 0 1 0,
+    Vector3 0 1 1,
+    Vector3 1 1 1,
+    Vector3 1 1 0
+    ]
+faceVerts Pz = [
+    Vector3 1 0 1,
+    Vector3 1 1 1,
+    Vector3 0 1 1,
+    Vector3 0 0 1
+    ]
+faceVerts Nx = [
+    Vector3 0 0 1,
+    Vector3 0 1 1,
+    Vector3 0 1 0,
+    Vector3 0 0 0
+    ]
+faceVerts Ny = [
+    Vector3 0 0 0,
+    Vector3 0 0 1,
+    Vector3 1 0 1,
+    Vector3 1 0 0
+    ]
+faceVerts Nz = [
+    Vector3 0 0 0,
+    Vector3 0 1 0,
+    Vector3 1 1 0,
+    Vector3 1 0 0
+    ]
+
+faceNormal :: Direction -> Vector3
+faceNormal Px = Vector3 1 0 0
+faceNormal Py = Vector3 0 1 0
+faceNormal Pz = Vector3 0 0 1
+faceNormal Nx = Vector3 (-1) 0 0
+faceNormal Ny = Vector3 0 (-1) 0
+faceNormal Nz = Vector3 0 0 (-1)
+
+faceTris :: [Word16]
+faceTris = [0,1,2,0,2,3]
+
+emptyMesh :: Mesh
+emptyMesh = Mesh {
+    mesh'vertexCount = 0,
+    mesh'triangleCount = 0,
+    mesh'vertices = [],
+    mesh'texcoords = [],
+    mesh'texcoords2 = Nothing,
+    mesh'normals = [],
+    mesh'tangents = Nothing,
+    mesh'colors = Nothing,
+    mesh'indices = Nothing,
+    mesh'animVertices = Nothing,
+    mesh'animNormals = Nothing,
+    mesh'boneIds = Nothing,
+    mesh'boneWeights = Nothing,
+    mesh'vaoId = 0,
+    mesh'vboId = Nothing
+}
+
+cubeMesh :: WindowResources -> IO Mesh
+cubeMesh = let
+    verts = directions >>= faceVerts
+    tris = faceTris |> replicate 6 |> zip [0..] >>= \(i,ts) -> ts <&> (+ (i * 4))
+    in uploadMesh (emptyMesh {
+        mesh'vertexCount = length verts,
+        mesh'triangleCount = length tris `div` 3,
+        mesh'vertices = verts,
+        mesh'texcoords = replicate (length verts) (Vector2 0 0),
+        mesh'normals = directions >>= replicate 4 <&> faceNormal,
+        mesh'indices = Just tris
+    }) False
 
 data State = State {
     frame :: !Int,
@@ -18,7 +104,7 @@ data State = State {
 }
 
 cubeModel :: WindowResources -> IO Model
-cubeModel w = genMeshCube 1 1 1 w >>= \cube -> loadModelFromMesh cube w
+cubeModel w = cubeMesh w >>= (`loadModelFromMesh` w)
 
 update :: State -> IO State
 update state = do
@@ -86,5 +172,5 @@ main = do
         450
         "h-craft"
         60
-        $ \w -> initialState w >>= \s -> whileWindowOpen_ (loop w) s
+        $ \w -> initialState w >>= whileWindowOpen_ (loop w)
 
