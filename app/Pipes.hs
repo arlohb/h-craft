58eb58d6c77c30a55ca1060ad5af2612c1b98adb
arlohb <arlo@urwick.co.uk>
2024-02-02 23:42:21 +0000

    Defining pipes with infix notation

--- app/Pipes.hs
+++ app/Pipes.hs
@@ -2,11 +2,11 @@ module Pipes where
 
 (|>) :: a -> (a -> b) -> b
 infixl 9 |>
-(|>) x f = f x
+x |> f = f x
 
 (||>) :: Functor f => f a -> (a -> b) -> f b
 infixl 9 ||>
-(||>) xs f = fmap f xs
+xs ||> f = fmap f xs
 
 (!?) :: [a] -> Int -> Maybe a
 infixl 9 !?
